2147af9b31910f9a29ad1d3a9640debc8b8d70ac
Brooke Hurford and Matt Rosanio <brookebhurford@gmail.com>
2016-01-12 15:35:09 -0800

    Add drawing functionality to the canvas

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 //Problem: No user interaction causes no change to application
 //Solution: When user interacts cause changes appropriately
-$(function() {
+//$(function() {
   var color = $('.selected').css("background-color");
+  var $canvas = $('canvas');
+  var context = $canvas[0].getContext("2d");
+  var lastEvent;
+  var mouseDown = false;
 
-  $('.controls li').click(function() {
+  $('.controls').on('click', 'li', function() {
     $(this).siblings().removeClass("selected");
     $(this).addClass("selected");
     color = $(this).css("background-color")
@@ -27,5 +31,32 @@ $(function() {
   //When the color slider change
   $("input[type=range]").change(changeColor);
 
+  //When add color is pressed
+  $('#addNewColor').click(function() {
+    var $newColor = $('<li></li>');
+    $newColor.css('background-color', $("#newColor").css("background-color"));
+    $('.controls ul').append($newColor);
+    $newColor.click();
+  });
 
-});
+  //On mouse events on the canvas
+  $canvas.mousedown(function(e) {
+    lastEvent = e;
+    mouseDown = true;
+  }).mousemove(function(e) {
+    if (mouseDown) {
+      context.beginPath();
+      context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+      context.lineTo(e.offsetX, e.offsetY);
+      context.strokeStyle = color;
+      context.stroke();
+      lastEvent = e;
+    }
+  }).mouseup(function() {
+    mouseDown=false;
+  }).mouseleave(function() {
+    $canvas.mouseup();
+  });
+    //Draw lines
+
+//});
